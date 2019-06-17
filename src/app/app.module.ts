bed399a81011e289ece81856a1d7baf7c14b9f4f
lmiletic <l.miletic@msgnetconomy.net>
2019-06-17 11:06:58 +0200

    Query parameters and fragments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,8 @@ const appRoutes: Routes = [
   { path: '', component: HomeComponent }, // localhost:4200
   { path: 'users', component: UsersComponent }, // localhost:4200/users
   { path: 'users/:id/:name', component: UserComponent }, // localhost:4200/users/:id/:name
-  { path: 'servers', component: ServersComponent } // localhost:4200/servers
+  { path: 'servers', component: ServersComponent }, // localhost:4200/servers
+  { path: 'servers/:id/edit', component: EditServerComponent } // localhost:4200/servers/:id/edit
 ];
 
 @NgModule({
